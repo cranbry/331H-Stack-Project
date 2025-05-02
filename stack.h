230f2aa8890a5d3efe6d33337ef0a2d9278ff582
Mouad Oulouali <mouadoune430@gmail.com>
2025-05-02 18:47:16 -0400

    added constructor, destructor and push method

--- stack.h
+++ stack.h
@@ -8,20 +8,31 @@ using namespace std;
 
 template <class T>
 struct Node {
-    T data;
-    Node<T> *next;
+    T data;            // node for storing data
+    Node<T> *next;     // pointer to next node
 };
 
 template <class T>
 class Stack {
     private:
-        Node <T> *top;
+        Node <T> *top; // pointer to the top node
     
     public:
+        // constructor for initializing empty stack
         Stack(): top(nullptr) {}
-
+        
+        // destructor for freeing dynamic memory
         ~Stack() {
         }
+
+    // method to push a value onto the top of th stack
+    void push(const T& value) {
+        // new node with given value that points to top
+        Node<T> *p = new Node<T>{value, top}; 
+
+        // updating top pointer to new node;
+        top = p;
+    }
 
 };
 
